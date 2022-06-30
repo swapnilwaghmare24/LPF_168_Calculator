97f04fb74f6d77abc51194bbdd5c7440c88547c2
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-06-30 21:40:01 +0530

    addition performed

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,14 @@
 public class Calculator()
 {
+	public double add(double num1,double num2)
+	{
+		return num1+num2;
+	}
+	public static void main(String[] args)
+	{
+		Calculator calculator= new Calculator();
+		double result=calculator.add(10.15,20.30);
+		System.out.println(result);
+	}
 
 }
